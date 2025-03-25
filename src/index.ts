2e7e4a63e3dd54c248b897e4cdcb8ad8e95a816f
duskalor <duskalor1@gmail.com>
2025-03-25 18:46:11 -0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,16 @@ import { phrases } from './phrases';
 // */30 * * * *  cada 30 minutos
 const app = () => {
   schedule('*/30 * * * * *', async () => {
-    console.log('enviando frase');
+    console.log(
+      `${new Intl.DateTimeFormat('default', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      }).format(new Date())} - enviando frase`
+    );
     const phrase = getPhrase(phrases);
     const tense = phrase.tense.replace(/([A-Z])/g, ' $1').trim();
     const message = `📚 **${tense}**\n\n💬 ${phrase.phrase}\n\n`;
